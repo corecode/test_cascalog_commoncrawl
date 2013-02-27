38afcc4c091d083dcb45dd86ab30c9ee21aec9a0
Simon Schubert <2@0x2c.org>
2013-02-27 15:00:26 +0100

    compress output files

--- src/test2/core.clj
+++ src/test2/core.clj
@@ -1,16 +1,27 @@
 (ns test2.core
   (:require [clojure.string :as s]
-            [cascalog.ops :as c])
+            [cascalog.ops :as c]
+            [cascalog.tap :as tap]
+            [cascalog.workflow :as w])
   (:use cascalog.api
         [cascalog.more-taps :only (hfs-wrtseqfile)])
   (:import [org.apache.hadoop.io Text]
-           [org.apache.commons.httpclient URI])
+           [org.apache.commons.httpclient URI]
+           [cascading.tuple Fields]
+           [cascading.scheme.hadoop TextLine TextLine$Compress])
   (:gen-class))
 
 (defn make-metadata-tap
   "Creates tap for common crawl metadata files."
   [path]
   (hfs-wrtseqfile path Text Text :outfields ["key" "value"]))
+
+(defn hfs-textline-compressed
+  [path & opts]
+  (let [scheme (->> (:outfields (apply array-map opts) Fields/ALL)
+                    (w/text-line ["line"]))]
+    (.setSinkCompression scheme TextLine$Compress/ENABLE)
+    (apply tap/hfs-tap scheme path opts)))
 
 (defmapop ^String parse-host
   "Return the host part of an URI."
@@ -38,9 +49,10 @@
      (let [metadata-tap (make-metadata-tap inpath)
            output-tap (if (= outpath "-")
                         (stdout)
-                        (hfs-textline outpath))
+                        (hfs-textline-compressed outpath))
            trap-tap (if (= outpath "-")
                       (stdout)
                       (hfs-seqfile (str outpath ".trap")))]
-       (?- output-tap
-           (query-domains metadata-tap trap-tap)))))
+       (with-job-conf {"mapred.output.compress" "true"}
+         (?- output-tap
+             (query-domains metadata-tap trap-tap))))))
